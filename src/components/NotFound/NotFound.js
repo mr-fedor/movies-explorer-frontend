d2e5314e8178aad2adfdbc13b6657460d0529106
Alex Blinov <cspp43@yandex.ru>
2021-11-28 17:17:22 +0300

    feature: add component app, header, notFound; add normalize.css

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import './NotFound.css';
+
+function NotFound() {
+  return (
+    <section className="not-found">
+      <div className="not-found__content">
+        <h1 className="not-found__title">404</h1>
+        <p className="not-found__desc">Страница не найдена</p>
+      </div>
+      <Link className="not-found__back" to="/">Назад</Link>
+    </section>
+  );
+}
+
+export default NotFound;
